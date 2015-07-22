08f3c27f44af90d47fa7612903baca0da59600d8
lindj0307 <lindj0307@gmail.com>
2015-07-22 17:53:12 +0800

    add some examples of <The programing of swift>

--- TheProgramingOfSwift/TheProgramingOfSwift/main.swift
+++ TheProgramingOfSwift/TheProgramingOfSwift/main.swift
@@ -1,7 +1,7 @@
 //
 //  main.swift
 //  TheProgramingOfSwift
-//
+//  Demo of the programing of swift
 //  Created by 林东杰 on 15/7/22.
 //  Copyright (c) 2015年 Anta. All rights reserved.
 //
@@ -83,7 +83,7 @@ print("\n")
 //集合类型
 //..数组:数组使用有序列表存储相同类型的多个数据,相同的值可以出现在一个数组的不同位置.
 //..语法:遵循Array<SomeType>这样的形式,其中sometype是这个数组中唯一允许存在的数据类型. 当然也可以使用 SomeType[]这样的简单语法.
-//....构造语句
+//....构造
 //之前是这样写 var shoppingList: String[] = ["Egg","Milk"] 现在这样写会报错
 var shoppingList: [String] = ["Egg","Milk"]
 //var shoppingList = ["Egg","Milk"]
@@ -123,11 +123,148 @@ var anotherThreeDoubles = Array(count: 3, repeatedValue: 2.5)
 println(anotherThreeDoubles)
 print("\n")
 
+//..字典:字典是一种存储相同类型多重数据的存储器.
+//..语法:使用Dictionary<KeyType,ValueType>定义,其中KeyType是字典中键的数据类型,ValueType是字典中对应于这些键所存储值的数据类型.
+//....构造
+var airports: Dictionary<String,String> = ["TYO": "Tokyo","DUB": "Dublin"]
+//var airports = ["TYO": "Tokyo","DUB": "Dublin"]
+//添加一个键值
+airports["LHR"] = "London"
+//修改一个键值
+airports["LHR"] = "London Heathrow"
+if let oldValue = airports.updateValue("Dublin Internation", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).\n")
+}
+airports["JON"] = "Know nothing John Snow"
+airports["APL"] = "Apple Internation"
+print(airports)
+print("\n")
+airports["APL"] = nil
+print(airports)
+print("\n")
+if let removedValue = airports.removeValueForKey("TYO") {
+    print("The removed airport's name is \(removedValue).\n")
+} else {
+    print("The airports dictionary does not contain a value for TYO.\n")
+}
+print(airports)
+print("\n")
+//....遍历字典
+for (airportCode,airportName) in airports {
+    print("\(airportCode):\(airportName)\n")
+}
+for airportCode in airports.keys {
+    print("airportCode:\(airportCode)\n")
+}
+for airportName in airports.values {
+    print("airportName:\(airportName)\n")
+}
+let airportCodes = Array(airports.keys)
+let airportNames = Array(airports.values)
+print(airportCodes)
+print("\n")
+print(airportNames)
+print("\n")
+//....创建一个空字典
+var nameOfInterges = Dictionary<Int,String>()
+nameOfInterges[16] = "sixteen"
+print(nameOfInterges)
+print("\n")
 
+//控制流 for for-in while do-while while-do switch if else -break -continue -fallthrough -return
+let someCharacter: Character = "e"
+switch someCharacter {
+    case "a","e","i","o","u":
+    print("\(someCharacter) is a vowel!\n")
+    case "b","c","d","f","g","h":
+    print("\(someCharacter) is a consonant!\n")
+    default: break
+}
+//..范围匹配
+let anotherCount = 3000000000000
+let countedThings = "stars in the Milky Way"
+var naturalCount: String
+switch anotherCount {
+case 0:
+    naturalCount = "no"
+case 1...3:
+    naturalCount = "a few"
+case 4...9:
+    naturalCount = "several"
+case 10...99:
+    naturalCount = "tens of"
+case 100...999:
+    naturalCount = "hundreds of"
+case 1000...99999:
+    naturalCount = "thousands of"
+default:
+    naturalCount = "no"
+}
+print("There are \(naturalCount) \(countedThings). \n")
+//..fallthrough
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2,3,5,7,11,13,17,19:
+    description += " a prime number, and alse"
+    fallthrough
+default:
+    description += " an integer."
+}
+print(description+"\n")
 
+//函数
+//..定义和调用函数
+func sayHello(personName: String) -> String {
+    return "Hello, " + personName + "!\n"
+}
+print(sayHello("Joey"))
+print(sayHello("John Snow"))
+//..多返回值函数
+func count(str: String) -> (vowels: Int, consonants: Int, others: Int) {
+    var vowels = 0,consonants = 0, others = 0
+    for character in str {
+        switch String(character).lowercaseString {
+            case "a","e","i","o","u":
+            ++vowels
+            case "b","c","d","f","g","h","j","k","l","m","n","q","r","s","t","v","w","x","y","z":
+            ++consonants
+            default:
+            ++others
+        }
+    }
+    return (vowels,consonants,others)
+}
 
+let total = count("You know nothing John Snow!")
+print("\(total.vowels) vowels and \(total.consonants) consonants and \(total.others) others!\n")
+//..函数的参数名    
+//....内部参数名称,与外部参数名称,在别人第一次阅读你代码不知道你函数参数目的的时候,就要考虑使用外部参数名了
+func join(s1: String, s2: String, joiner: String) -> String {
+    return s1 + joiner + s2 + "\n"
+}
+print(join("hello","Joey",","))
 
+//带外部参数并且提供默认值
+func anotherJoin(string s1: String, toString s2: String, withJoiner joiner: String = " ") -> String {
+     return s1 + joiner + s2 + "\n"
+}
+print(anotherJoin(string: "Hello", toString: "John", withJoiner: ","))
+print(anotherJoin(string: "Hello", toString: "John"))
 
+func containCharacter(#string: String, #characterToFind: Character) -> Bool {
+    for character in string {
+        if character == characterToFind {
+            return true
+        }
+    }
+    return false
+}
+let containV = containCharacter(string: "sfsdfdsf", characterToFind: "v")
+print(containV)
+print("\n")
+
+//可变参数
 
 
 
