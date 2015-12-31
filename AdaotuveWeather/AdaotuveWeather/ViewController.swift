53482dfcb836a05f4c419b4e34896a37d8d317a2
lindongjie <lindj0307@gmail.com>
2015-12-31 23:45:06 +0800

    add project AdaptiveWeather

---
+++ AdaotuveWeather/AdaotuveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaotuveWeather
+//
+//  Created by 林东杰 on 12/31/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
