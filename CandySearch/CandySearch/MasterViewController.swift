c23c75b4b0ece66a16b77657c47683ac12b9dd2c
lindongjie <lindj0307@gmail.com>
2016-01-15 18:24:18 +0800

    finish Candy Search

--- CandySearch/CandySearch/MasterViewController.swift
+++ CandySearch/CandySearch/MasterViewController.swift
@@ -24,59 +24,148 @@ import UIKit
 
 class MasterViewController: UITableViewController {
   
-  // MARK: - Properties
-  var detailViewController: DetailViewController? = nil
-  var candies = [Candy]()
-  
-  // MARK: - View Setup
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    if let splitViewController = splitViewController {
-      let controllers = splitViewController.viewControllers
-      detailViewController = (controllers[controllers.count - 1] as! UINavigationController).topViewController as? DetailViewController
-    }
-  }
-  
-  override func viewWillAppear(animated: Bool) {
-    clearsSelectionOnViewWillAppear = splitViewController!.collapsed
-    super.viewWillAppear(animated)
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
-  // MARK: - Table View
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return candies.count
-  }
-  
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    // MARK: - Variable
     
-    let candy = candies[indexPath.row]
-    cell.textLabel!.text = candy.name
-    cell.detailTextLabel!.text = candy.category
-    return cell
-  }
-  
-  // MARK: - Segues
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "showDetail" {
-      if let indexPath = tableView.indexPathForSelectedRow {
-        let candy = candies[indexPath.row]
-        let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-        controller.detailCandy = candy
-        controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem()
-        controller.navigationItem.leftItemsSupplementBackButton = true
-      }
+
+    // MARK: - Properties
+    var detailViewController: DetailViewController? = nil
+    var candies = [Candy]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredCandies = [Candy]()
+    
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        candies = [Candy(category: "Chocolate", name: "Chocolate Bar"),
+            Candy(category: "Chocolate", name: "Chocolate Chip"),
+            Candy(category: "Chocolate", name: "Dark Chocolate"),
+            Candy(category: "Hard", name: "Lollipop"),
+            Candy(category: "Hard", name: "Jaw Breaker"),
+            Candy(category: "Hard", name: "Candy Cane"),
+            Candy(category: "Other", name: "Caramel"),
+            Candy(category: "Other", name: "Sour Chew"),
+            Candy(category: "Other", name: "Gummi Bear")
+        ]
+        
+        if let splitViewController = splitViewController {
+            let controllers = splitViewController.viewControllers
+            detailViewController = (controllers[controllers.count - 1] as! UINavigationController).topViewController as? DetailViewController
+        }
+        
+        searchController.searchBar.scopeButtonTitles = ["All","Chocolate","Hard","Other"]
+        searchController.searchBar.delegate = self
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
+
     }
-  }
+    
+    override func viewWillAppear(animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Table View
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.active && searchController.searchBar.text != "" {
+            return filteredCandies.count
+        }
+        return candies.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let candy : Candy //candies[indexPath.row]
+        if  searchController.active && searchController.searchBar.text != "" {
+            candy = filteredCandies[indexPath.row]
+        } else {
+            candy = candies[indexPath.row]
+        }
+        
+        cell.textLabel!.text = candy.name
+        cell.detailTextLabel!.text = candy.category
+        return cell
+    }
+    
+    // MARK: - Segues
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let candy : Candy 
+                if  searchController.active && searchController.searchBar.text != "" {
+                    candy = filteredCandies[indexPath.row]
+                } else {
+                    candy = candies[indexPath.row]
+                }
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailCandy = candy
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+    // MARK: - TableView Datasource
+    
+    // MARK: - TableView Delegate
+    
+    // MARK: - Custom Delegate
+    
+    // MARK: - Event Response
+    
+    // MARK: - Private Methods
+    func filterContentForSearchText(searchText: String, scope: String = "All" ) {
+        filteredCandies = candies.filter {
+            candy in
+            let categoryMatch = (scope == "All") || (candy.category == scope)
+            return categoryMatch && candy.name.lowercaseString.containsString(searchText.lowercaseString)
+        }
+        tableView.reloadData()
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+        filterContentForSearchText(searchController.searchBar.text!,scope: scope)
+    }
+    
+    // MARK: - Navigation
   
+
 }
+
+extension MasterViewController: UISearchResultsUpdating {
+    func updatSearchResultsForSearchControll(searchController: UISearchController) {
+    let searchBar = searchController.searchBar
+    let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+    filterContentForSearchText(searchController.searchBar.text!, scope: scope)
+    }
+}
+
+
+
+extension MasterViewController: UISearchBarDelegate {
+    // MARK: - UISearchBar Delegate
+    func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+    }
+}
+
+
+
+
+
 
