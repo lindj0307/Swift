1a20a648a7d4d3c1c8d192654e6b4bf70615ad5a
lindongjie <lindj0307@gmail.com>
2016-01-19 12:03:27 +0800

    MathMonsters
    add project MathMonsters

---
+++ MathMonsters/MathMonsters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathMonsters
+//
+//  Created by 林东杰 on 1/19/16.
+//  Copyright © 2016 Joey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
